fdd5fa5f10472f9d4187e072a42f848be58864fe
Alpha Hinex <AlphaHinex@gmail.com>
2021-02-15 21:38:25 +0800

    Have a try

---
+++ scripts/g.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const googleIt = require('google-it');
+const handler = module.exports = {};
+
+handler.response = (msg) => {
+    if (msg.startsWith('g ') || msg.startsWith('G ')) {
+        googleIt({'query': msg.substr(2)}).then(results => {
+            // access to results object here
+            let res = 'Search results:';
+            results.forEach(result => {
+                res += '\r\n\r\n' + result.title;
+                res += '\r\n' + result.href;
+                res += '\r\n' + result.snippet;
+            });
+            handler.envelop(res);
+        }).catch(e => {
+            // any possible errors that might have occurred (like no Internet connection)
+        });
+    }
+};
