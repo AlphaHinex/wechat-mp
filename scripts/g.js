debe89a20849dfe21ff78012223d2c49679c713c
Alpha Hinex <AlphaHinex@gmail.com>
2021-02-15 23:29:22 +0800

    Works

--- scripts/g.js
+++ scripts/g.js
@@ -2,11 +2,16 @@
 
 const googleIt = require('google-it');
 const handler = module.exports = {};
+const options = {
+    headers: {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36',
+        'accept-language': 'zh-CN,zh;q=0.9,en;q=0.8'
+    }
+};
 
 handler.response = (msg) => {
     if (msg.startsWith('g ') || msg.startsWith('G ')) {
-        googleIt({'query': msg.substr(2)}).then(results => {
-            console.debug(results);
+        googleIt({'query': msg.substr(2), 'disableConsole': true, limit: 5}).then(results => {
             // access to results object here
             let res = 'Search results:';
             results.forEach(result => {
