dcc3f97765c3e7d49480ad8a3d30d8435f9a5a50
Alpha Hinex <AlphaHinex@gmail.com>
2021-02-17 23:07:30 +0800

    Add debug info

--- scripts/qr.js
+++ scripts/qr.js
@@ -14,17 +14,27 @@ handler.response = (msg) => {
                 console.debug(data);
                 const addUrl = 'https://api.weixin.qq.com/cgi-bin/material/add_material?access_token=' + token;
                 const options = {
-                    method: 'POST'
+                    method: 'POST',
+                    headers: {
+                        'Content-Length': data.length
+                    }
                 };
                 const req = https.request(addUrl, options, (res) => {
+                    console.log(`STATUS: ${res.statusCode}`);
+                    console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
                     let addRes = ''
                     res.on('data', (buf) => addRes += buf.toString());
                     res.on('end', () => {
-                        console.debug('On end: ' + addRes);
-                        // const json = JSON.parse(addRes.toString());
-                        // console.debug("Add media response: " + json);
+                        console.debug(`add res: ${addRes}`);
+                        const json = JSON.parse(addRes.toString());
+                        console.debug("Add media response: " + json);
                     });
                 });
+
+                req.on('error', (e) => {
+                    console.error(`problem with request: ${e.message}`);
+                });
+
                 req.write(data);
                 req.end();
             });
