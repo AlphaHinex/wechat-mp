2dd9dd009fd29f46525729c170396adfc194adc0
Alpha Hinex <AlphaHinex@gmail.com>
2022-09-02 11:17:37 +0800

    Move to lark

--- scripts/hsa-faq.js
+++ scripts/hsa-faq.js
@@ -6,6 +6,6 @@ const io = require('../io');
 handler.response = (msg) => {
     if (msg.startsWith('hsa-faq ')) {
         io.server.emit('hsa-faq', msg.substr(8));
-        handler.envelop('请加入钉钉群（34796091）接收消息！');
+        handler.envelop('请加入飞书群 HSA FAQ（https://www.feishu.cn/download?share_chat_token=be9jbc24-0998-4b86-b872-3d2baeb06007）接收消息！');
     }
 };
